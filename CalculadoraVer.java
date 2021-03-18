690aa7138b162a122b4b129ad1979b0b3b78065a
ibaicolegio <77358044+ibaicolegio@users.noreply.github.com>
2021-03-18 11:47:21 +0100

    Add files via upload

---
+++ CalculadoraVer.java
@@ -0,0 +1,29 @@
+
+public class CalculadoraVer {
+
+	private int n1, n2;
+
+	public CalculadoraVer(int n1, int n2) {
+		super();
+		this.n1 = n1;
+		this.n2 = n2;
+	}
+	
+	public int suma(){
+		return n1+n2;
+	}
+	
+	public int resta(){
+		return n1-n2;
+	}
+	
+	public int multi(){
+		return n1*n2;
+	}
+	
+	public int divide(){
+		return n1/n2;
+	}
+	
+	
+}
